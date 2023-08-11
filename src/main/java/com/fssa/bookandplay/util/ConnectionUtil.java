906fe6e129dde5b57af31c3e9e71569fa567fabe
Sandeepsan97 <116252886+Sandeepsan97@users.noreply.github.com>
2023-08-11 06:22:29 +0530

    Add files via upload

---
+++ src/main/java/com/fssa/bookandplay/util/ConnectionUtil.java
@@ -0,0 +1,91 @@
+package com.fssa.bookandplay.util;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.fssa.bookandplay.exceptions.DAOException;
+
+import io.github.cdimascio.dotenv.Dotenv;
+/**
+ * The ConnectionUtil Class has a method  get connection which help to connect  with database
+ */
+public class ConnectionUtil {
+	
+	private ConnectionUtil() {
+	// private constructor
+	}
+	/**
+	 * Creating a Logger Class For Display Message
+	 * @throws DAOException 
+	 */
+	static Logger logger = new Logger();
+	public static Connection getConnection() throws DAOException, SQLException {
+        Connection con = null;
+
+        String url;
+        String userName;
+        String passWord;
+
+        if (System.getenv("CI") != null) {
+            url = System.getenv("DATABASE_HOST");
+            userName = System.getenv("DATABASE_USERNAME");
+            passWord = System.getenv("DATABASE_PASSWORD");
+            
+        } else {
+            Dotenv env = Dotenv.load();
+            url = env.get("DATABASE_HOST");
+            userName = env.get("DATABASE_USERNAME");
+            passWord = env.get("DATABASE_PASSWORD");
+        }
+
+        try {
+        	Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, userName, passWord);
+           logger.info("connected");
+        } catch (Exception e) {
+           
+            throw new DAOException("Unable to connect to the database");
+        }
+        
+        finally {
+        	
+        	logger.info("get connection method is executed");
+        }
+        return con;
+    }
+	public static void close(Connection conn, Statement stmt, ResultSet rs,PreparedStatement ps,CallableStatement cs) throws DAOException {
+
+		try {
+		
+			if (rs != null) {
+				rs.close();
+			}
+			if(cs!=null) {
+				cs.close();
+			}
+			if(ps!=null) {
+				ps.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			throw new  DAOException("Unable to close to the database");
+			/**
+			 *  No need re throw the exception.
+			 */
+		
+		}
+	}
+
+
+
+}
---
+++ src/test/java/com/fssa/bookandplay/service/TestGroundService.java
@@ -0,0 +1,72 @@
+package com.fssa.bookandplay.service;
+
+import java.sql.SQLException;
+
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.fssa.bookandplay.exceptions.DAOException;
+import com.fssa.bookandplay.model.Ground;
+
+class TestGroundService {
+
+	@Test
+	void testAddGround() throws DAOException, SQLException {
+		Ground ground = getGround2();
+
+		Assertions.assertTrue(GroundService.addGround(ground));
+
+	}
+
+	@Test
+	void testUpdateGround() throws DAOException, SQLException {
+		Ground ground = getGround2();
+
+		Assertions.assertTrue(GroundService.updateGround(ground));
+
+	}
+
+	@Test
+	void testDeleteGround() throws DAOException, SQLException {
+
+		Assertions.assertTrue(GroundService.deleteGround(37));
+
+	}
+
+	 Ground getGround2() {
+		List<String> validImages = Arrays.asList("https://example.com/image1.jpg", "https://example.com/image2.jpg");
+		List<String> validsports = Arrays.asList("cricket", "football", "tennis");
+		LocalTime startTime = LocalTime.of(10, 30); // 10:00 AM
+		LocalTime endTime = LocalTime.of(11, 30); // 5:00 PM
+		Ground ground = new Ground(4, "saple name", "sample main area", "sampleaddress", "http://google.com",
+				"sampledistrict", validImages, validsports, startTime, endTime, "samplerules", 200, 200, 3);
+		return ground;
+
+	}
+
+	 /**
+	Ground getGround() {
+		List<String> validImages = Arrays.asList("https://example.com/image1.jpg", "https://example.com/image2.jpg");
+		List<String> validsports = Arrays.asList("cricket", "football", "tennis");
+		LocalTime startTime = LocalTime.of(10, 30); // 10:00 AM
+		LocalTime endTime = LocalTime.of(11, 30); // 5:00 PM
+		Ground ground = new Ground("sample name", "sample main area", "sampleaddress", "http://google.com",
+				"sampledistrict", validImages, validsports, startTime, endTime, "samplerules", 200, 200, 3);
+		return ground;
+
+	}
+	*/
+
+
+	@Test
+	void testGetGroundDetail() throws DAOException, SQLException {
+
+		Assertions.assertTrue(GroundService.getGroundDetails());
+
+	}
+
+}
